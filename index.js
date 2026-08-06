986de89fb19bb0d2edc7a9dcbe93986ad44c18d6
bilung1995 <redmibali95@gmail.com>
2026-08-07 04:56:41 +0700

    Rename subscription import to subscription.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
 
-const subscription = require("./subscribe");
+const subscription = require("./subscription");
 
 // ======================================================
 // ENV
